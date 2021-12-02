cf53306678f5bf1ee35979d95d7d2ca2e1d6a556
Juliano Boese <juliano.boese@gmail.com>
2021-12-02 17:16:33 -0300

    Requisito 14

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputEmail = document.getElementById('email');
 const inputSenha = document.getElementById('password');
 const formButton = document.getElementById('submit');
+const rateValues = document.getElementById('rate-container');
 
 formButton.addEventListener('click', () => {
   if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
@@ -9,3 +10,20 @@ formButton.addEventListener('click', () => {
     alert('Email ou senha inválidos.');
   }
 });
+
+window.onload = function addRateValues() {
+  for (let i = 1; i <= 10; i += 1) {
+    const valueToAdd = document.createElement('input');
+    valueToAdd.type = 'radio';
+    valueToAdd.name = 'rate';
+    valueToAdd.value = i;
+    valueToAdd.id = 'rate' + i;
+
+    const labelToAdd = document.createElement('label');
+    labelToAdd.setAttribute('for','rate' + i);
+    labelToAdd.innerText = i;
+
+    rateValues.appendChild(valueToAdd);
+    rateValues.appendChild(labelToAdd);
+  }
+}

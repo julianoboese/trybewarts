5afb1d4cc7d7fce43bc9027804bc67efa5eaed06
Juliano Boese <juliano.boese@gmail.com>
2021-12-02 21:00:23 -0300

    Ajusta nomes e posições de variáveis

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const inputEmail = document.getElementById('email');
-const inputSenha = document.getElementById('password');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 const formButton = document.getElementById('submit');
 const rateValues = document.getElementById('rate-container');
 const agreementCheck = document.getElementById('agreement');
@@ -8,7 +8,7 @@ const counter = document.getElementById('counter');
 const textarea = document.getElementById('textarea');
 
 formButton.addEventListener('click', () => {
-  if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
@@ -17,17 +17,17 @@ formButton.addEventListener('click', () => {
 
 window.onload = function addRateValues() {
   for (let i = 1; i <= 10; i += 1) {
-    const valueToAdd = document.createElement('input');
-    valueToAdd.type = 'radio';
-    valueToAdd.name = 'rate';
-    valueToAdd.value = i;
-    valueToAdd.id = `rate${i}`;
+    const inputToAdd = document.createElement('input');
+    inputToAdd.type = 'radio';
+    inputToAdd.name = 'rate';
+    inputToAdd.value = i;
+    inputToAdd.id = `rate${i}`;
 
     const labelToAdd = document.createElement('label');
     labelToAdd.setAttribute('for', `rate${i}`);
     labelToAdd.innerText = i;
 
-    rateValues.appendChild(valueToAdd);
+    rateValues.appendChild(inputToAdd);
     rateValues.appendChild(labelToAdd);
   }
 };
@@ -46,29 +46,26 @@ textarea.addEventListener('keyup', () => {
   counter.innerText = disponiveis;
 });
 
+const respostaNome = document.getElementById('answer-name');
+const inputName = document.getElementById('input-name');
+const inputLastName = document.getElementById('input-lastname');
+const respostaEmail = document.getElementById('answer-email');
+const inputEmail = document.getElementById('input-email');
+const respostaCasa = document.getElementById('answer-house');
+const selectCasa = document.getElementById('house');
+const respostaFamilia = document.getElementById('answer-family');
+const familias = document.getElementsByClassName('family');
+const respostaObs = document.getElementById('answer-obs');
+
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  const respostaNome = document.getElementById('respostas-nome');
-  const inputName = document.getElementById('input-name');
-  const inputLastName = document.getElementById('input-lastname');
   respostaNome.innerText = `${respostaNome.innerText} ${inputName.value} ${inputLastName.value}`;
-
-  const respostaEmail = document.getElementById('respostas-email');
-  const inputEmail = document.getElementById('input-email');
   respostaEmail.innerText += ` ${inputEmail.value}`;
-
-  const respostaCasa = document.getElementById('respostas-casa');
-  const selectCasa = document.getElementById('house');
   respostaCasa.innerText += ` ${selectCasa.value}`;
-
-  const respostaFamilia = document.getElementById('respostas-familia');
-  const familias = document.getElementsByClassName('family');
   for (let i = 0; i < familias.length; i += 1) {
     if (familias[i].checked) {
       respostaFamilia.innerText += ` ${familias[i].value}`;
     }
   }
-
-  const respostaObs = document.getElementById('respostas-obs');
   respostaObs.innerText += ` ${textarea.value}`;
 });
